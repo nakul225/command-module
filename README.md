2650d7364389f7bf5ca4bdd246ad88a35ca04efb
Nakul <nakul@linuxmint.linuxmint-domain>
2014-03-12 04:00:38 -0400

    Started making changes to add Alfred

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ command-module
 ==============
 
 A productivity webapp made with Flask
+
+Reference: Built on example code given at link: https://github.com/mitsuhiko/flask/tree/master/examples/minitwit
+
---
+++ alfred.py
@@ -0,0 +1,15 @@
+#This file contains all functions relevant to processing natural language command entered by the user
+import sys
+
+def process_command(input_command):
+    """
+    Perform some NLP on the command to figure out the type of function that needs to be called on input.
+    For example,
+    Do we call:
+    1) Google calendar
+    2) Wolfram API
+    3) Google search
+    4) Nutrition API
+    5) some other cool API
+    """
+    return "yo man wassup"
